00deb85464b4337427cfeb8c4a779b491295fcb2
Leonardo Nogueira da Silva <leonardo.lns@outlook.com>
2022-05-24 16:30:22 -0300

    aws storage

--- src/Aws.Run/Program.cs
+++ src/Aws.Run/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Amazon.Runtime.CredentialManagement;
+using Aws.Commons.Configurations;
+
+var configuration = new AwsConfiguration("", "bucket", "key", "secrete", "default", "account id");
+
+var options = new CredentialProfileOptions
+{
+    AccessKey = configuration.AccessKey,
+    SecretKey = configuration.SecretKey
+};
+var profile = new CredentialProfile(configuration.ProfileName, options);
+var sharedFile = new SharedCredentialsFile();
+sharedFile.RegisterProfile(profile);
+
+
+//chamada de codigo
